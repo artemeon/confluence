66258ec17c9313a5c081ee1ea0231f4e37d7ca0a
MarkoWystrach <marko.wystrach@artemeon.de>
2025-08-29 15:09:32 +0200

    #26447: render yellow panels

--- src/MacroReplacer/InfoBoxReplacer.php
+++ src/MacroReplacer/InfoBoxReplacer.php
@@ -8,25 +8,28 @@ class InfoBoxReplacer implements MacroReplacerInterface
 {
     public function replace(string $haystack): string
     {
-        $haystack = preg_replace_callback(
-            '/<div class="(info|note|tip|warning)"[^>]*>\s*<ac:rich-text-body>(.*?)<\/ac:rich-text-body>\s*<\/div>/is',
+        return preg_replace_callback(
+            '/<div class="(info|panel)"[^>]*>(.*?)<\/div>/is',
             function ($matches) {
                 $type = strtolower($matches[1]);
                 $content = $matches[2];
+                $boxStyling = match ($type) {
+                    'info' => $this->infoBoxStyling(),
+                    'panel' => $this->warningBoxStyling(),
+                    default => '%s',
+                };
 
-                if (preg_match('/^\s*<p[^>]*>(.*?)<\/p>\s*$/is', $content, $inner)) {
+                if (preg_match('/<ac:rich-text-body[^>]*>(.*?)<\/ac:rich-text-body>/is', $content, $inner)) {
                     $content = $inner[1];
                 }
 
                 return sprintf(
-                    $this->infoBoxStyling(),
+                    $boxStyling,
                     $content
                 );
             },
             $haystack
         );
-
-        return $haystack;
     }
 
     private function infoBoxStyling(): string
@@ -34,6 +37,18 @@ class InfoBoxReplacer implements MacroReplacerInterface
         return '
         <div class="flex flex-row items-start bg-blue-100 gap-2 p-4">
             <agp-icon name="info-circle" size="lg" type="duotone" class="mt-1 [--fa-primary-color:var(--color-white)] [--fa-secondary-color:var(--color-primary-500)] [--fa-secondary-opacity:1]"></agp-icon>
+            <div>%s</div>
+        </div>
+        ';
+    }
+
+    private function warningBoxStyling(): string
+    {
+        return '
+        <div class="flex flex-row items-start bg-yellow-100 gap-2 px-4">
+            <p>
+                <agp-icon name="lightbulb" size="lg" type="duotone" class="mt-1" styles="--fa-primary-color:var(--color-gray-500);--fa-secondary-opacity:1;--fa-secondary-color:var(--color-yellow-700);"></agp-icon>
+            </p>
             <div>%s</div>
         </div>
         ';
