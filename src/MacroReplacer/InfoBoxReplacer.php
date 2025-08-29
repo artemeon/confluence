08ab1820ea9ffc8706c025e521b9dd357c0303e1
MarkoWystrach <marko.wystrach@artemeon.de>
2025-08-29 15:26:44 +0200

    #26447: refactor

--- src/MacroReplacer/InfoBoxReplacer.php
+++ src/MacroReplacer/InfoBoxReplacer.php
@@ -13,46 +13,38 @@ class InfoBoxReplacer implements MacroReplacerInterface
             function ($matches) {
                 $type = strtolower($matches[1]);
                 $content = $matches[2];
-                $boxStyling = match ($type) {
-                    'info' => $this->infoBoxStyling(),
-                    'panel' => $this->warningBoxStyling(),
-                    default => '%s',
-                };
+                switch ($type) {
+                    case 'panel':
+                        $backgroundColor = 'bg-yellow-100';
+                        $iconHtml = '<agp-icon name="lightbulb" size="xl" type="duotone" class="mt-1" styles="--fa-primary-color:var(--color-gray-500);--fa-secondary-opacity:1;--fa-secondary-color:var(--color-yellow-700);"></agp-icon>';
+                        break;
+                    case 'info':
+                        $backgroundColor = 'bg-blue-100';
+                        $iconHtml = '<agp-icon name="info-circle" size="xl" type="duotone" class="mt-1" styles="--fa-primary-color:var(--color-white);--fa-secondary-opacity:1;--fa-secondary-color:var(--color-blue-700);"></agp-icon>';
+                        break;
+                    default:
+                        $backgroundColor = '';
+                        $iconHtml = '';
+                        break;
+                }
 
                 if (preg_match('/<ac:rich-text-body[^>]*>(.*?)<\/ac:rich-text-body>/is', $content, $inner)) {
                     $content = $inner[1];
                 }
 
                 return sprintf(
-                    $boxStyling,
+                    '
+                    <div class="flex flex-row items-start %s gap-2 px-4">
+                        <p>%s</p>
+                        <div>%s</div>
+                    </div>
+                    ',
+                    $backgroundColor,
+                    $iconHtml,
                     $content
                 );
             },
             $haystack
         );
-    }
-
-    private function infoBoxStyling(): string
-    {
-        return '
-        <div class="flex flex-row items-start bg-blue-100 gap-2 px-4">
-            <p>
-                <agp-icon name="info-circle" size="xl" type="duotone" class="mt-1" styles="--fa-primary-color:var(--color-white);--fa-secondary-opacity:1;--fa-secondary-color:var(--color-blue-700);"></agp-icon>
-            </p>
-            <div>%s</div>
-        </div>
-        ';
-    }
-
-    private function warningBoxStyling(): string
-    {
-        return '
-        <div class="flex flex-row items-start bg-yellow-100 gap-2 px-4">
-            <p>
-                <agp-icon name="lightbulb" size="xl" type="duotone" class="mt-1" styles="--fa-primary-color:var(--color-gray-500);--fa-secondary-opacity:1;--fa-secondary-color:var(--color-yellow-700);"></agp-icon>
-            </p>
-            <div>%s</div>
-        </div>
-        ';
     }
 }
