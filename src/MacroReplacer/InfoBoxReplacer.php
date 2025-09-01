93b97a624c8dba90dee2c16cfa38e5de5d2faa3d
MarkoWystrach <marko.wystrach@artemeon.de>
2025-09-01 14:30:26 +0200

    #26672: remove tailwind/agp styling

--- src/MacroReplacer/InfoBoxReplacer.php
+++ src/MacroReplacer/InfoBoxReplacer.php
@@ -13,20 +13,6 @@ class InfoBoxReplacer implements MacroReplacerInterface
             function ($matches) {
                 $type = strtolower($matches[1]);
                 $content = $matches[2];
-                switch ($type) {
-                    case 'panel':
-                        $backgroundColor = 'bg-yellow-100';
-                        $iconHtml = '<agp-icon name="lightbulb" size="xl" type="duotone" class="mt-1" styles="--fa-primary-color:var(--color-gray-500);--fa-secondary-opacity:1;--fa-secondary-color:var(--color-yellow-700);"></agp-icon>';
-                        break;
-                    case 'info':
-                        $backgroundColor = 'bg-blue-100';
-                        $iconHtml = '<agp-icon name="info-circle" size="xl" type="duotone" class="mt-1" styles="--fa-primary-color:var(--color-white);--fa-secondary-opacity:1;--fa-secondary-color:var(--color-blue-700);"></agp-icon>';
-                        break;
-                    default:
-                        $backgroundColor = '';
-                        $iconHtml = '';
-                        break;
-                }
 
                 if (preg_match('/<ac:rich-text-body[^>]*>(.*?)<\/ac:rich-text-body>/is', $content, $inner)) {
                     $content = $inner[1];
@@ -34,13 +20,11 @@ class InfoBoxReplacer implements MacroReplacerInterface
 
                 return sprintf(
                     '
-                    <div class="flex flex-row items-start %s gap-2 px-4 mb-4">
-                        <p>%s</p>
+                    <div class="confluence-%s">
                         <div>%s</div>
                     </div>
                     ',
-                    $backgroundColor,
-                    $iconHtml,
+                    $type,
                     $content
                 );
             },
